c181bfab3b9916ac99c1d20775fafe0fc068843e
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-05-18 20:08:35 +0530

    add room model

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,13 +1,10 @@
 import express from "express";
+import { login, register } from "../controllers/auth-controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello, this is auth endpoint!");
-})
+router.post("/register", register)
 
-router.get("/register", (req, res) => {
-    res.send("Hello, this is auth endpoint!");
-})
+router.post("/login", login)
 
 export default router
