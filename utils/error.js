cc4cdc3ecc572d4a3bcdc01febe83976dcc62b24
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-05-24 21:25:48 +0530

    error fix

--- utils/error.js
+++ utils/error.js
@@ -1,8 +1,8 @@
 export const createError = (status, message) => {
     
     const err = new Error();
-    err.status = status;
-    err.message = message;
+    // err.status = status;
+    // err.message = message;
     return err;
 
 }
